4a882ec7b40fc10d18cb785a8a3b3ff8b6675b27
Sri Harsha Patnala <90171810+harshapatnala@users.noreply.github.com>
2021-10-11 23:42:29 -0400

    Add files via upload

---
+++ project_benches/decode/tb/tests/decode_test_pkg.sv
@@ -0,0 +1,13 @@
+package decode_test_pkg;
+
+	import uvm_pkg::*;
+	import uvmf_base_pkg_hdl::*;
+	import uvmf_base_pkg::*;
+	`include "uvm_macros.svh"
+	import decode_in_pkg::*;
+	import decode_out_pkg::*;
+	
+	`include "src/print_component.svh"
+	`include "src/decode_test.svh"
+
+endpackage
